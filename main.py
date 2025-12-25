a15edac77b9ad7ed820beb32a34ac658047a310c
ycstevenwong <ycstevenwong@gmail.com>
2025-12-25 20:40:05 +0800

    my version of sort for dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars;
+from stats import count_words, count_chars, sort_chars;
 def get_book_text(file):
     with open(file) as f:
         # f is a file object
@@ -6,12 +6,19 @@ def get_book_text(file):
     return file_contents
 
 def main():
-    file_path = "./books/frankenstein.txt"
+    file_path = "books/frankenstein.txt"
     file_contents = get_book_text(file_path)
     words = count_words(file_contents);
     chars = count_chars(file_contents);
+    sorted_chars = sort_chars(chars);
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {len(words)} total words")
-    print(chars);
+    print("--------- Character Count -------")
+    for key,value in sorted_chars.items():
+        print(f"{key}: {value}");
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -3,11 +3,15 @@ def count_words(file_contents):
     return words;
 
 def count_chars(file_contents):
-    chars_list = {};
+    chars_dict = {};
     full_contents = file_contents.lower();
     for char in full_contents:
-        if char not in chars_list:
-            chars_list[char] = 1;
+        if char not in chars_dict:
+            chars_dict[char] = 1;
         else:
-            chars_list[char] += 1;
-    return chars_list;
+            chars_dict[char] += 1;
+    return chars_dict;
+
+def sort_chars(chars_dict):
+    sorted_dict = dict(sorted(chars_dict.items(), key=lambda item: item[1],reverse=True))
+    return sorted_dict;
