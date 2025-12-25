9eaa8fcc170a22e8833c9a84190ddaaf4a79f375
ycstevenwong <ycstevenwong@gmail.com>
2025-12-25 19:58:42 +0800

    create main.py and stats.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from stats import count_words;
+def get_book_text(file):
+    with open(file) as f:
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_path = "./books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
+    words = count_words(file_contents);
+    print(f"Found {len(words)} total words")
+
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(file_contents):
+    words = file_contents.split();
+    return words;
